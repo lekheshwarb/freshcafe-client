edeb56d4dbe9b008f54c890d5b124c6236c380c3
lekheshwarb <lekheshwar.bejjarapu@freshworks.com>
2022-09-15 22:56:29 +0530

    Orders Filters

--- app/components/orders/orders-main.js
+++ app/components/orders/orders-main.js
@@ -1,4 +1,23 @@
-import Component from '@ember/component';
+import Component from "@ember/component";
+import { get, set, computed } from "@ember/object";
 
 export default Component.extend({
+  filter: null,
+
+  updatedOrders: computed("filter", function () {
+    let updatedList = this.orders;
+    if (this.get("filter") !== null) {
+      updatedList = [];
+      this.orders.forEach((order) => {
+        if (order.status === this.get("filter")) updatedList.push(order);
+      });
+    }
+    return updatedList;
+  }),
+
+  actions: {
+    changeFilter(type) {
+      this.set("filter", type);
+    },
+  },
 });
--- app/components/orders/orders-modal.js
+++ app/components/orders/orders-modal.js
@@ -2,4 +2,13 @@ import Component from "@ember/component";
 
 export default Component.extend({
   tagName: "",
+
+  init() {
+    this._super(...arguments);
+    let sum = 0;
+    this.orderItems.forEach((orderItem) => {
+      sum += orderItem.item_price;
+    });
+    this.set("totalAmount", sum);
+  },
 });
